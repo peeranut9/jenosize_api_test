216e546795c1caad93a32c584166b82f5a9bcfb3
Peeranut Khattiyapathomchai <peeranut.park@gmail.com>
2023-04-24 16:39:38 +0700

    dev.js

---
+++ functions/dev.js
@@ -0,0 +1,6 @@
+import app from "./app.js";
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`API listening on port ${port}`);
+});
--- functions/functions/gamexo/botMove.js
+++ functions/functions/gamexo/botMove.js
@@ -28,23 +28,27 @@ export default (board) => {
     }
   }
   const emptySquares = [];
+  const listPlayerCross = [];
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (board[i][j] === "") {
         emptySquares.push({ row: i, col: j });
+      } else if (board[i][j] === "X" && (i == 1 || j == 1)) {
+        listPlayerCross.push({ row: i, col: j });
       }
     }
   }
-  if (board[1][1] === "O") {
+
+  const connerList = emptySquares.filter(
+    (e) => [0, 2].includes(e.row) && [0, 2].includes(e.col)
+  );
+  if (board[1][1] === "O" && listPlayerCross.length < 2) {
     const crossList = emptySquares.filter((e) => e.row == 1 || e.col == 1);
     if (crossList.length > 0) {
       const randomIndex = Math.floor(Math.random() * crossList.length);
       return { ...crossList[randomIndex] };
     }
   }
-  const connerList = emptySquares.filter(
-    (e) => [0, 2].includes(e.row) && [0, 2].includes(e.col)
-  );
   if (connerList.length > 0) {
     const randomIndex = Math.floor(Math.random() * connerList.length);
     return { ...connerList[randomIndex] };
