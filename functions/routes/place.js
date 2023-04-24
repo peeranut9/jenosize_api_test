fe9ffb3f41c0504f6345205153b2eb56c9c82e16
Peeranut Khattiyapathomchai <peeranut.park@gmail.com>
2023-04-24 23:58:25 +0700

    route by lat lang

--- functions/routes/place.js
+++ functions/routes/place.js
@@ -17,22 +17,12 @@ router.get("/", function (req, res) {
           res.json({
             next_page_token,
             results: results.map((e) => {
-              let storeLink = null;
-              try {
-                const linkRegex = /href="([^"]*)"/;
-                const linkMatch =
-                  e.photos[0].html_attributions[0].match(linkRegex);
-                if (linkMatch) {
-                  storeLink = linkMatch[1];
-                }
-              } catch (error) {}
               return {
                 name: e.name,
-                link: storeLink
-                  ? storeLink
-                  : e.geometry && e.geometry.location
-                  ? `https://www.google.com/maps/search/?api=1&query=${e.geometry.location.lat},${e.geometry.location.lng}`
-                  : undefined,
+                link:
+                  e.geometry && e.geometry.location
+                    ? `https://www.google.com/maps/search/?api=1&query=${e.geometry.location.lat},${e.geometry.location.lng}`
+                    : undefined,
                 image:
                   e.photos && e.photos.length > 0
                     ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${e.photos[0].photo_reference}&key=${process.env.GOOGLE_API_KEY}`
